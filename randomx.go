8af3d77b0bb72c14209236f6fbaddd2b53d517c5
Command <maoxs2@gmail.com>
2020-07-04 11:46:11 +0800

    no static flag for macos

--- randomx.go
+++ randomx.go
@@ -1,10 +1,10 @@
 package randomx
 
 //#cgo CFLAGS: -I./randomx
-//#cgo LDFLAGS: -lrandomx -lstdc++ -static
+//#cgo LDFLAGS: -lrandomx -lstdc++
 //#cgo linux,amd64 LDFLAGS:-L${SRCDIR}/build/linux-x86_64 -lm
 //#cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/build/macos-x86_64 -lm
-//#cgo windows,amd64 LDFLAGS:-L${SRCDIR}/build/windows-x86_64 -static-libgcc -static-libstdc++
+//#cgo windows,amd64 LDFLAGS:-L${SRCDIR}/build/windows-x86_64 -static -static-libgcc -static-libstdc++
 /*
 #include "randomx.h"
 #include <time.h>
