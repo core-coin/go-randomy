15938b27e0de1f59fcfa85fe797be4eba2986b46
Moein <moein.mirzaei95@gmail.com>
2021-05-19 20:21:10 +0430

    Add librandomx.a for android-arm64

--- randomy.go
+++ randomy.go
@@ -6,6 +6,7 @@ package randomy
 //#cgo linux,arm64 LDFLAGS:-L${SRCDIR}/build/linux-arm64 -lm
 //#cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/build/macos-x86_64 -lm
 //#cgo windows,amd64 LDFLAGS:-L${SRCDIR}/build/windows-x86_64
+//#cgo android,arm64 LDFLAGS:-L${SRCDIR}/build/anroid-arm64 -lm
 //#include <stdlib.h>
 //#include "randomy.h"
 import "C"
