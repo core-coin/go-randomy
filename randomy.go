2ed414f6785e4b07f668f2f5e2da89b942fc53b5
Sasha Zezulinsky <alexander.zezulinsky@gmail.com>
2022-02-13 02:14:56 +0100

    randomy

--- randomy.go
+++ randomy.go
@@ -4,8 +4,8 @@ package randomy
 //#cgo LDFLAGS: -lrandomx -lstdc++
 //#cgo linux,amd64 LDFLAGS:-L${SRCDIR}/build/linux-x86_64 -lm
 //#cgo linux,arm64 LDFLAGS:-L${SRCDIR}/build/linux-arm64 -lm
-//#cgo darwin,amd64 CGO_CFLAGS:-mmacosx-version-min=10.12
-//#cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/build/darwin-x86_64 -lm
+//#cgo darwin,amd64 CFLAGS:-mmacosx-version-min=10.8
+//#cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/build/darwin-x86_64 -lm -mmacosx-version-min=10.8
 //#cgo windows,amd64 LDFLAGS:-L${SRCDIR}/build/windows-x86_64
 //#include <stdlib.h>
 //#include "randomy.h"
