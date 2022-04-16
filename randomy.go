368bbb59d393fe999be57e21203736e9287f52fc
error2215 <slyvkamm@gmail.com>
2022-04-16 19:22:23 +0200

    -lm

--- randomy.go
+++ randomy.go
@@ -1,9 +1,9 @@
 package randomy
 
 //#cgo CFLAGS: -I./randomx
-//#cgo linux,amd64 LDFLAGS:-L${SRCDIR}/build/linux-x86_64 -lm -lrandomx -lstdc++
-//#cgo linux,arm64 LDFLAGS:-L${SRCDIR}/build/linux-arm64 -lm -lrandomx -lstdc++
-//#cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/build/darwin-x86_64 -lm -lrandomx -lstdc++
+//#cgo linux,amd64 LDFLAGS: -lrandomx -lstdc++ -L${SRCDIR}/build/linux-x86_64 -lm
+//#cgo linux,arm64 LDFLAGS: -lrandomx -lstdc++ -L${SRCDIR}/build/linux-arm64 -lm
+//#cgo darwin,amd64 LDFLAGS: -lrandomx -lstdc++ -L${SRCDIR}/build/darwin-x86_64 -lm
 //#cgo windows,amd64 LDFLAGS:-L${SRCDIR}/build/windows-x86_64 -lm -static-libstdc++ -static-libgcc -lrandomx
 //#include <stdlib.h>
 //#include "randomy.h"
